a8a2cbd309bb8abcc804838b331ddcd8170fa343
Adrian Moses <adrian@adrianmoses.com>
2025-08-23 22:10:36 +0200

    add initial files for cookiecutter

---
+++ {{ cookiecutter.project_name }}/src/config/settings.py
@@ -0,0 +1,26 @@
+# {{cookiecutter.project_slug}}/src/config/settings.py
+from pydantic_settings import BaseSettings
+from typing import List, Optional
+
+
+class Settings(BaseSettings):
+    project_name: str = "{{cookiecutter.project_name}}"
+    description: str = "{{cookiecutter.description}}"
+
+    # Database settings
+
+    # API settings
+    api_host: str = "0.0.0.0"
+    api_port: int = 8000
+
+    # Data sources
+    # data_sources: List[str] = {{cookiecutter.data_sources}}
+
+    # NLP settings
+    spacy_model: str = "en_core_web_sm"
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
